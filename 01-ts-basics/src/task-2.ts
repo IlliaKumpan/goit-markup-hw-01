c6d3566ae37a34466b28911d3ef267b097bbce03
I.Kumpan <ilyakumpan11@gmail.com>
2026-03-15 18:02:48 +0200

    init

---
+++ 01-ts-basics/src/task-2.ts
@@ -0,0 +1,13 @@
+interface product{
+    readonly id: number;
+    description?: string;
+ }
+
+const product = {
+  id: 1,
+  title: "Tablet",
+  description: "Compact and fast",
+};
+
+console.log(`Product: ${JSON.stringify(product)}`);
+
